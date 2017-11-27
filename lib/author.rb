23f966d7be3b6d762328da3fd9433ae7881f9894
Steven Senkowski <scsenkowski@gmail.com>
2017-11-27 17:27:35 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,9 +3,14 @@ class Author
 
   @@post_count = 0
 
+  def initialize(post)
+    @post = post
+    @posts = []
+  end
 
   def add_post(post)
-    
+    @song = post
+
     @@post_count += 1
   end
 
