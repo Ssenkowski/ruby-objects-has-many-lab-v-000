d2467bb249ba5ab766c222afa5c085590d602c6c
Steven Senkowski <scsenkowski@gmail.com>
2017-11-27 17:22:54 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,18 @@
 class Author
   attr_accessor :posts, :name
 
+  @@post_count = 0
+
 
   def add_post(post)
-
+    
   end
 
+  def add_post_by_title(post_title)
+      
+  end
 
+  def post_count
+    @@post_count
+  end
 end
