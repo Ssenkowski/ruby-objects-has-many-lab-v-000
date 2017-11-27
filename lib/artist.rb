cc64735a85b15704a475dac83c1d2745298cef21
Steven Senkowski <scsenkowski@gmail.com>
2017-11-27 17:24:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,20 @@
+class Artist
+  attr_accessor :songs, :name
 
+  @@song_count = 0
+
+
+  def add_post(post)
+
+    @@song_count += 1
+  end
+
+  def add_post_by_title(post_title)
+
+    @@song_count += 1
+  end
+
+  def post_count
+    @@song_count
+  end
+end
